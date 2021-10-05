d681f25b9eda801423f696629f2e6f85205e8ba9
prahalad <prahaladhazarika1@gmail.com>
2021-10-05 10:39:31 +0530

    added some folders and theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 
 function App() {
-  return <MainLayout></MainLayout>;
+  return (
+    <ThemeProvider theme={theme}>
+      {/* dry run your component inside here */}
+    </ThemeProvider>
+  );
 }
 
 export default App;
