3092f7ea57fe246f9c8e8e16b32f5cd2baf6058f
Govind Bisen <govind.bisen@gmail.com>
2021-10-05 16:56:26 +0530

    implementation of calDate component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
+import CalDate from "./components/calDate";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* dry run your component inside here */}
+      <CalDate />
     </ThemeProvider>
   );
 }
---
+++ src/components/calDate.js
@@ -0,0 +1,30 @@
+
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import Stack from '@mui/material/Stack';
+
+export default function CalDate() {
+  const [value, setValue] = React.useState(new Date());
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Stack spacing={3}>
+       
+        <DesktopDatePicker
+          label="Date"
+          value={value}
+          minDate={new Date('2017-01-01')}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </Stack>
+    </LocalizationProvider>
+  );
+}
+
+
+
