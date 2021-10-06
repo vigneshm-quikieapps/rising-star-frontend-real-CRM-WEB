ee27ce9fe3375346c34e801400b0f855e71b96ce
shubhzade <shubhamzade1697@gmail.com>
2021-10-06 16:55:43 -0700

    IconBtn_comp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "./styles/global.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
 import Dropdown from "./components/Dropdown"
+import  IconButton from './components/IconButton'
+
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+
+
 
 function App() {
+const a=<AccountBalanceIcon/>
+
   return (
     <ThemeProvider theme={theme}>
-      <Dropdown/>
+      <IconButton label={a}/>
     </ThemeProvider>
   );
 }
 
 export default App;
+
+
+
