78ef707cd215a3af9afdf3fb94a97e174424ec18
shubhzade <shubhamzade1697@gmail.com>
2021-10-05 17:27:05 -0700

    dropdown_and_tabcomponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
+import Dropdown from "./components/Dropdown"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      {/* dry run your component inside here */}
+      <Dropdown/>
     </ThemeProvider>
   );
 }
