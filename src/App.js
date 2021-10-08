8d52cebc399c11993b67bfc558b30926ae3548ef
Abbas <abbas@quikieapp.com>
2021-10-08 15:44:15 +0330

    remove other team members stuff

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 import theme from "./styles/theme";
 import Classes from "./containers/class-list";
-import Dropdown from "./components/Dropdown";
-import IconButton from "./components/IconButton";
-import TabComponent from "./components/TabComponent";
-import CheckBox from "./components/CheckBox";
-import InputFieldOne from "./components/InputFieldOne";
-import InputFieldTwo from "./components/InputFieldTwo";
-import Popup from "./components/PopOne";
 
 import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
@@ -19,14 +12,6 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* dry run your component inside here */}
-      {/* <IconButton label={a} /> */}
-      {/* <Dropdown label="select country" /> */}
-      {/* <TabComponent btnText="New Tab"/> */}
-      {/* <CheckBox /> */}
-      {/* <InputFieldOne /> */}
-      {/* <InputFieldTwo /> */}
-      {/* <Popup /> */}
       <Classes />
     </ThemeProvider>
   );
