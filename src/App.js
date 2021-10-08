ce217f910343b8f779c80af78a2b2d35175d9be6
Abbas <abbas@quikieapp.com>
2021-10-08 20:26:11 +0330

    add notifications component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 import theme from "./styles/theme";
 import Classes from "./containers/class-list";
-
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import Notifications from "./components/notifications";
+import { Box } from "@mui/system";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  const a = <AccountBalanceIcon />;
-
   return (
     <ThemeProvider theme={theme}>
-      <Classes />
+      <CssBaseline />
+
+      {/* <Classes /> */}
+      <Box sx={{ textAlign: "center" }}>
+        <Notifications
+          show={true}
+          items={[
+            "notification 1",
+            "notification 2",
+            "notification 3",
+            "notification 4",
+            "notification 5",
+            "notification 6",
+          ]}
+          sx={{ marginLeft: "300px" }}
+        ></Notifications>
+      </Box>
     </ThemeProvider>
   );
 }
