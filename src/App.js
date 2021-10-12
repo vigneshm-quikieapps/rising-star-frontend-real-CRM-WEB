74654b535e28c2ef9df56194f7ef7cf0c7a97ea4
Kartikey Pandey <kartikey@quikieapps.com>
2021-10-12 13:22:51 +0530

    checkbox implementation using MUI theme is done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
-import CheckBox from "./components/CheckboxStyled";
+import StyledCheckbox from "./components/StyledCheckbox";
 import { useState } from "react";
 
 function App() {
   const [checked, setChecked] = useState(true);
-
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
   return (
     <ThemeProvider theme={theme}>
-      <CheckBox checked={checked} setChecked={setChecked} />
+      <StyledCheckbox onChange={handleChange} checked={checked} />
+
       {/* dry run your component inside here */}
     </ThemeProvider>
   );
---
+++ src/components/StyledCheckbox.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import { styled } from "@mui/material/styles";
+import CheckIcon from "@mui/icons-material/Check";
+
+const StyledCheckIcon = styled(CheckIcon)({
+  width: "18px",
+  height: "18px",
+  color: "#fff",
+});
+
+const CheckboxTheme = styled(Checkbox)(({ theme }) => {
+  console.log(theme);
+  return {
+    "&.Mui-checked": {
+      backgroundImage: "linear-gradient(133deg, #ff1a6d, #ff6e2d 100%)",
+      borderColor: "transparent !important",
+    },
+    "&.MuiCheckbox-root": {
+      width: "30px",
+      height: "28px",
+      borderRadius: "8px",
+      border: `1px solid ${theme.palette.checkbox.border}`,
+      backgroundColor: theme.palette.checkbox.background,
+      color: "transparent",
+    },
+  };
+});
+
+const StyledCheckbox = ({ ...rest }) => {
+  return <CheckboxTheme checkedIcon={<StyledCheckIcon />} {...rest} />;
+};
+
+export default StyledCheckbox;
