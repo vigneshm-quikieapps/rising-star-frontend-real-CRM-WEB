72263318a27a8c9f7cff08a03f3d7b4ff704cc80
prahalad <prahaladhazarika1@gmail.com>
2021-10-12 18:09:49 +0530

    input and dropdown

--- src/components/PopOne.jsx
+++ src/components/PopOne.jsx
@@ -1,41 +1,38 @@
 import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
-const PopUp = styled("div")(({ theme }) => ({
-  position: "absolute",
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.highlight.main,
+    color: theme.palette.text.secondary,
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+    borderRadius: theme.spacing(1),
+  },
 
-  borderRadius: "8px",
-  boxShadow: "1px 2px 6px grey",
-  padding: "10px",
-  "& .text-container": {
-    position: "relative",
-    "& .text": {
-      margin: 0,
-    },
-  },
-  "& .text-container::before": {
-    content: '""',
-    position: "absolute",
-    width: "20px",
-    height: "20px",
-    borderBottom: "0px",
-    borderRight: "0px",
-    left: "82%",
-    top: "-19px",
-    marginLeft: "-25px",
-    transform: "rotate(45deg)",
-    backgroundColor: "white",
-    marginBottom: "-40px",
-    borderRadius: "4px",
-  },
+  // [`& .${tooltipClasses.tooltip}::before`]: {
+  //   // backgroundColor: theme.palette.common.white,
+  //   // color: "rgba(0, 0, 0, 0.87)",
+  //   // boxShadow: theme.shadows[1],
+  //   // fontSize: 11,
+  //   // borderRadius: theme.spacing(1),
+  //   content: '""',
+  //   position: "absolute",
+  //   width: "20px",
+  //   height: "20px",
+  //   borderBottom: "0px",
+  //   borderRight: "0px",
+  //   // bottom: "100%",
+  //   top: "1px",
+  //   left: "50%",
+  //   // marginLeft: "-25px",
+  //   transform: "rotate(45deg)",
+  //   backgroundColor: "white",
+  //   // marginBottom: "-40px",
+  //   borderRadius: "10px",
+  // },
 }));
 
-export default function PopOne(props) {
-  const { text } = props;
-  return (
-    <PopUp>
-      <div className="text-container">
-        <h4 className="text">{text}</h4>
-      </div>
-    </PopUp>
-  );
-}
+export default LightTooltip;
---
+++ src/components/inputfield.jsx
@@ -0,0 +1,106 @@
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+
+const style1 = (theme) => {
+  return {
+    width: "100%",
+    "& .Mui-focused::after": {
+      content: '""',
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      borderRadius: "8px",
+      border: "1px solid transparent" /*2*/,
+      "-webkit-mask":
+        "linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)",
+      "-webkit-mask-composite": "destination-out" /*5'*/,
+      "mask-composite": "exclude",
+      background: theme.palette.gradients.diagonal,
+    },
+
+    "& .MuiOutlinedInput-root": {
+      outline: "none",
+      "& fieldset": {
+        borderColor: "grey",
+        borderRadius: 8,
+      },
+      "&:hover fieldset": {
+        borderColor: "grey",
+      },
+      "&.Mui-focused fieldset": {
+        border: "none",
+      },
+    },
+
+    "& label.Mui-focused": {
+      "&::after": {
+        border: "none",
+        content: '""',
+        background: "white",
+      },
+
+      padding: "0px 10px",
+      color: "#ff1a6d",
+      background: "white",
+      letterSpacing: "0.9px",
+    },
+  };
+};
+
+const style2 = (theme) => {
+  return {
+    width: "100%",
+    borderRadius: 8,
+    outline: "1px solid #e2e2e1",
+    "& .Mui-focused::after": {
+      content: '""',
+      position: "absolute",
+      height: "inherit",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      borderRadius: "8px",
+      border: "1px solid transparent" /*2*/,
+      "-webkit-mask":
+        "linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0)",
+      "-webkit-mask-composite": "destination-out" /*5'*/,
+      "mask-composite": "exclude",
+      background: theme.palette.gradients.diagonal,
+    },
+    "& .MuiFilledInput-root": {
+      borderRadius: 8,
+
+      backgroundColor: "white",
+      transition: theme.transitions.create([
+        "border-color",
+        "background-color",
+        "box-shadow",
+      ]),
+      "&:hover": {
+        backgroundColor: "transparent",
+      },
+      "&.Mui-focused": {
+        border: "none",
+        background: "white",
+      },
+    },
+    "& label.Mui-focused": {
+      "&::after": {
+        border: "none",
+        content: '""',
+        background: "white",
+      },
+      color: "#ff1a6d",
+      background: "inherit",
+    },
+  };
+};
+
+const InputField = styled(TextField)(({ theme, variant }) =>
+  variant === "outlined" ? style1(theme) : style2(theme)
+);
+
+export default InputField;
