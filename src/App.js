1dd6b9ec45400f26327970410ef00437ad7c9ddd
Abbas <abbas@quikieapp.com>
2021-10-13 23:49:49 +0330

    remove extra lines from App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/configureStore";
 
-// import MainLayout from "./hoc/main-layout";
+import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 
 function App() {
@@ -16,47 +16,3 @@ function App() {
 }
 
 export default App;
-
-<<<<<<< HEAD
-{
-  /* <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DatePicker
-          label="Pay Month"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => (
-            <StyledTexField variant="filled" {...params} />
-          )}
-        />
-      </LocalizationProvider>
-      <br />
-      <br />
-      <br />
-      <br />
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DatePicker
-          label="Pay Month"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => (
-            <StyledTexField variant="outlined" {...params} />
-          )}
-        />
-      </LocalizationProvider>
-      <br />
-      <br />
-      <br />
-      <br />
-      <StyledCheckbox onChange={handleChange} checked={checked} /> */
-}
-=======
-
-// display: "flex",
-// flexDirection: "row",
-// justifyContent: "space-around",
-
->>>>>>> shubham
