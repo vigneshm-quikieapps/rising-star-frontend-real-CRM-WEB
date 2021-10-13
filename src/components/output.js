8d16f0129d8c169022f5b16023fe41ad8d236a43
Abbas <abbas@quikieapp.com>
2021-10-13 22:26:21 +0330

    fix output component issues

--- src/components/output.js
+++ src/components/output.js
@@ -1,65 +1,11 @@
 import * as React from "react";
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { typography } from "@mui/system";
-// import { typography } from "@mui/system";
-// import Box from "@mui/material/Box";
-// import { typography } from '@mui/system';
 
-// const StyledBox = styled(Typography)(({ theme }) => ({
-//   width: 880,
-//   height: 200,
-//   // bgcolor: "#f2f1f6",
-//   borderRadius: "20px",
-//   border: "solid 1px #f2f1f6",
-//   display: "flex",
-//   flexDirection: "row",
-//   justifyContent: "space-around",
-//   alignItems: "flex-end",
-//   "& .textstyle": {
-//     display: "flex",
-//     flexDirection: "row",
-//     justifyContent: "space-around",
-//     fontSize: "12px",
-//     opacity: "0.5",
-//   },
-//   "& .desciptionStyle ": {
-//     fontSize: "14px",
-//   },
-// }));
-
-// const OutputBox = ({ title, description }) => {
-//   return (
-//     <StyledBox>
-//       <Typography variant="h4">Pre-school gymnastics (Age: 1-3)</Typography>
-
-//       <typography>
-//         <Box className="textstyle">Class ID</Box>
-//         <Box className="desciptionStyle">12345566</Box>
-//       </typography>
-
-//       <typography>
-//         <Box className="textstyle">postcode</Box>
-//         <Box className="desciptionStyle">342678</Box>
-//       </typography>
-
-//       <typography>
-//         <Box className="textstyle">city/town</Box>
-//         <Box className="desciptionStyle">london</Box>
-//       </typography>
-
-//       <typography>
-//         <Box className="textstyle">Status</Box>
-//         <Box className="desciptionStyle">Active</Box>
-//       </typography>
-//     </StyledBox>
-//   );
-// };
-
-const StyledBox = styled(Box)(({ theme, varient }) => ({
-  display: "inline-flex",
+const StyledBox = styled(Box)({
+  display: "flex",
   flexDirection: "column",
-}));
+});
 
 const Output = ({ title, description, variant = "default" }) => {
   return (
@@ -67,7 +13,6 @@ const Output = ({ title, description, variant = "default" }) => {
       <Typography
         sx={{
           fontSize: "12px",
-          fontWeight: "bold",
           color: (theme) => theme.palette.text.secondary,
         }}
       >
@@ -77,7 +22,8 @@ const Output = ({ title, description, variant = "default" }) => {
       {typeof description === "string" ? (
         <Typography
           sx={{
-            fontSize: variant === "title" ? "16px" : "12px",
+            fontSize: variant === "header" ? "16px" : "14px",
+            fontWeight: variant === "header" ? "bold" : "normal",
             color: (theme) => theme.palette.text.primary,
           }}
         >
