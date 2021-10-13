c15de9e2c4abd4b37496c4d826a9f5f0ef53378c
Abbas <abbas@quikieapp.com>
2021-10-13 14:58:03 +0330

    remove extra code from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
-import { HomeOutlined } from "@mui/icons-material";
 
 import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
 
-import IconButton from "./components/icon-button";
-
 function App() {
   return (
     <BrowserRouter>
-      <MainLayout>
-        {/* dry run your component inside here */}
-        <IconButton>
-          <HomeOutlined />
-        </IconButton>
-      </MainLayout>
+      <MainLayout>{/* dry run your component inside here */}</MainLayout>
     </BrowserRouter>
   );
 }
