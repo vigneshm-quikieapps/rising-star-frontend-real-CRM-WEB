cb725b3986395fd0b47514663e6cde991a75eb0b
“arun <arunsaras48.2@gmail.com>
2021-10-14 23:35:58 +0530

    fetch class url changes

--- src/helper/config.js
+++ src/helper/config.js
@@ -2,7 +2,7 @@ export const BASE_DOMAIN = "https://ismart-rising-star.herokuapp.com/";
 export const BASE_URL = BASE_DOMAIN + "api/";
 
 const API = {
-  GetBusinessClassListAPI: BASE_URL + "classes",
+  GetBusinessClassListAPI: BASE_URL + "business/",
 };
 
 export default API;
--- src/redux/action/classAction.js
+++ src/redux/action/classAction.js
@@ -1,5 +1,5 @@
 import { GET_CLASS_LIST_SAGA } from "../types";
 
-export const getClassList = () => {
-  return { type: GET_CLASS_LIST_SAGA };
+export const getClassList = (id) => {
+  return { type: GET_CLASS_LIST_SAGA, payload: id };
 };
