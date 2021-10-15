559b84843047c0408934e744a2edfc7ef68b1359
Abbas <abbas@quikieapp.com>
2021-10-15 16:51:45 +0330

    fix button heights

--- src/components/gradient-button.jsx
+++ src/components/gradient-button.jsx
@@ -1,16 +1,13 @@
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-
 const GradientButton = styled(Button, {
   shouldForwardProp: (prop) => prop !== "discard",
-})(({ theme,discard }) => ({
-
-  backgroundImage:discard? "#fff" : theme.palette.gradients.horizontalLinear,
-  color:discard? "#ff2c60":"#fff",
-
-  // 1=8px  2=16px
-  padding: theme.spacing(1, 2),
+})(({ theme, discard }) => ({
+  backgroundImage: discard ? "#fff" : theme.palette.gradients.horizontalLinear,
+  color: discard ? "#ff2c60" : "#fff",
+  height: "48px",
+  padding: "0px 25px",
   fontSize: theme.shape.borderRadius.secondary,
   border: `1px solid ${theme.palette.highlight.main}`,
   borderRadius: theme.shape.borderRadius.ternary,
