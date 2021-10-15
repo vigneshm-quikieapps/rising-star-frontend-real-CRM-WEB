27c2f09986b687aab16d63f460ef81f3d7d3b6d7
Abbas <abbas@quikieapp.com>
2021-10-15 17:05:57 +0330

    add initial classes page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import store from "./redux/store/configureStore";
 
 import MainLayout from "./hoc/main-layout";
 import "./styles/global.css";
+import Classes from "./pages/classes";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <MainLayout>
+          <Classes />
           {/* dry run your component inside here */}
         </MainLayout>
       </BrowserRouter>
