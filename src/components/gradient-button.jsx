d7fd50f9092e70a1c39106d6f321a898cdc4b5bf
Abbas <abbas@quikieapp.com>
2021-10-20 00:17:30 +0330

    add size prop to gradient button

--- src/components/gradient-button.jsx
+++ src/components/gradient-button.jsx
@@ -1,14 +1,16 @@
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
+const sizes = { normal: "16px", large: "20px" };
+
 const GradientButton = styled(Button, {
-  shouldForwardProp: (prop) => prop !== "discard",
-})(({ theme, discard }) => ({
+  shouldForwardProp: (prop) => ["discard", "size"].indexOf(prop) === -1,
+})(({ theme, discard, size = "normal" }) => ({
   backgroundImage: discard ? "#fff" : theme.palette.gradients.horizontalLinear,
   color: discard ? "#ff2c60" : "#fff",
   height: "48px",
   padding: "0px 25px",
-  fontSize: theme.shape.borderRadiuses.secondary,
+  fontSize: sizes[size],
   border: `1px solid ${theme.palette.highlight.main}`,
   borderRadius: theme.shape.borderRadiuses.ternary,
 }));
