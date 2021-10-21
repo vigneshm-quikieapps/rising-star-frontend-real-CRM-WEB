0b06987ee9ad985df00f0d978a78ad7d8e58114c
Abbas <abbas@quikieapp.com>
2021-10-21 09:22:39 +0330

    remove console.log

--- src/services/businesses-service.js
+++ src/services/businesses-service.js
@@ -4,7 +4,6 @@ export async function getBusinesses() {
   try {
     const response = await axiosInstance.get("/businesses");
     const businessList = response.data.docs;
-    console.log(businessList);
     return businessList;
   } catch (error) {
     throw error;
