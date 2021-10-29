0c89c144fcd001a1376f6e0c49bb0b24158cbd25
Abbas <abbas@quikieapp.com>
2021-10-29 12:46:13 +0330

    adopt with breaking changes in the back-end

--- src/services/class-services.js
+++ src/services/class-services.js
@@ -2,9 +2,10 @@ import axiosInstance from "../utils/axios-instance";
 
 export async function getClasses(params) {
   try {
-    const response = await axiosInstance.get("/auth/user/classes", { params });
-    const classList = response.data.docs;
-    const { totalPages, page: currentPage } = response.data;
+    const response = await axiosInstance.get("classes/of-logged-in-user", {
+      params,
+    });
+    const { docs: classList, totalPages, page: currentPage } = response.data;
     return { classList, totalPages, currentPage };
   } catch (error) {
     throw error;
