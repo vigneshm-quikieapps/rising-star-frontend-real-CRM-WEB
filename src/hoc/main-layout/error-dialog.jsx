fff8b69047b7ab665235df0d5425794267fc5891
Abbas Hosseini <hosseini44444@gmail.com>
2021-11-11 15:24:13 +0330

    resolve unmounting issue while error happens

--- src/hoc/main-layout/error-dialog.jsx
+++ src/hoc/main-layout/error-dialog.jsx
@@ -11,32 +11,29 @@ import {
 
 import { removeError } from "../../redux/action/shared-actions";
 
-const ErrorDialog = ({ children }) => {
+const ErrorDialog = () => {
   const dispatch = useDispatch();
   const errors = useSelector((state) => state.shared.errors);
-  if (!errors.length) return children;
+  if (!errors.length) return null;
   const removeErrorHandler = () => {
     dispatch(removeError());
   };
   return (
-    <>
-      <Dialog
-        open={!!errors.length}
-        onClose={removeErrorHandler}
-        sx={{ zIndex: (theme) => theme.zIndex.modal + 1 }}
-      >
-        <DialogTitle>The following error occurred</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <Typography component="pre">{errors[0]}</Typography>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={removeErrorHandler}>Discard</Button>
-        </DialogActions>
-      </Dialog>
-      {children}
-    </>
+    <Dialog
+      open={!!errors.length}
+      onClose={removeErrorHandler}
+      sx={{ zIndex: (theme) => theme.zIndex.modal + 1 }}
+    >
+      <DialogTitle>The following error occurred</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <Typography component="pre">{errors[0]}</Typography>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={removeErrorHandler}>Discard</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
