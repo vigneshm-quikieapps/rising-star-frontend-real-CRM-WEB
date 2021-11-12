06593ba8926ec87be4eaa62712ec8931184a04ab
Abbas Hosseini <hosseini44444@gmail.com>
2021-11-12 11:24:50 +0330

    disable error bopundary in development

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -15,36 +15,37 @@ import MainLayout from "../hoc/main-layout";
 
 const MainRouter = () => (
   <BrowserRouter>
-    <ErrorBoundary>
-      <Switch>
-        <Route path="/login" exact>
-          <Login />
-        </Route>
-        <Redirect from="/setup" to="/setup/term" exact />
-        <Route>
-          <MainLayout>
-            <Route path="/" exact>
-              <Dashboard />
-            </Route>
-            <Route path="/classes">
-              <Classes />
-            </Route>
-            <Route path="/members">
-              <Members />
-            </Route>
-            <Route path="/setup/term" exact>
-              <SetupTerm />
-            </Route>
-            <Route path="/setup/paymentUpload" exact>
-              <SetupPaymentUpload />
-            </Route>
-          </MainLayout>
-        </Route>
-        <Route>
-          <NoMatch />
-        </Route>
-      </Switch>
-    </ErrorBoundary>
+    {/* /// enable it for production */}
+    {/* <ErrorBoundary> */}
+    <Switch>
+      <Route path="/login" exact>
+        <Login />
+      </Route>
+      <Redirect from="/setup" to="/setup/term" exact />
+      <Route>
+        <MainLayout>
+          <Route path="/" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/classes">
+            <Classes />
+          </Route>
+          <Route path="/members">
+            <Members />
+          </Route>
+          <Route path="/setup/term" exact>
+            <SetupTerm />
+          </Route>
+          <Route path="/setup/paymentUpload" exact>
+            <SetupPaymentUpload />
+          </Route>
+        </MainLayout>
+      </Route>
+      <Route>
+        <NoMatch />
+      </Route>
+    </Switch>
+    {/* </ErrorBoundary> */}
   </BrowserRouter>
 );
 
