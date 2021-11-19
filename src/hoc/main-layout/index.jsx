5933583c3c2aca8f595d01f17ed74abbb718e573
Abbas Hosseini <hosseini44444@gmail.com>
2021-11-19 10:42:13 +0330

    add logo to side nav

--- src/hoc/main-layout/index.jsx
+++ src/hoc/main-layout/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { styled } from "@mui/material/styles";
-import { Divider, Typography } from "@mui/material";
+import { Link as MuiLink, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { getBusinessList } from "../../redux/action/businesses-actions";
@@ -9,32 +9,42 @@ import Header from "./header";
 import Footer from "./footer";
 import SideNav from "./side-nav";
 import Main from "./main";
+import logo from "../../assets/images/Logo.png";
 import { navItems } from "../../helper/constants";
 import ErrorDialog from "./error-dialog";
 
 const drawerWidth = 270;
 
-const DrawerHeader = styled("div")(({ theme }) => ({
+const DrawerHeader = styled(Link)(({ theme }) => ({
+  margin: "0 auto 60px 16px",
   display: "flex",
   padding: theme.spacing(2, 1),
   justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  textDecoration: "none",
 }));
 
 const NavHeader = (
   <>
-    <DrawerHeader>
+    <DrawerHeader to="/">
+      <img
+        src={logo}
+        alt="Rising Starts Logo - Go to home page"
+        width="133"
+        height="32"
+      />
       <Typography
-        component={Link}
-        to="/"
         sx={{
-          color: (theme) => theme.palette.text.navItemActive,
+          color: "#fff8",
           textDecoration: "none",
+          fontSize: "11px",
         }}
       >
         Rising Stars
       </Typography>
     </DrawerHeader>
-    <Divider />
+    {/* <Divider /> */}
   </>
 );
 
