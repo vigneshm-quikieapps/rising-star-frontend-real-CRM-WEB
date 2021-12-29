0e31f064a6787027d8161223b4e1e17ac1c63725
Abbas Hosseini <hosseini44444@gmail.com>
2021-12-29 18:15:04 +0530

    add add TableHeading component, add isLoading, isFetching, containerProps and headerCellWidth props to the custom table component

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -6,13 +6,19 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
+  CircularProgress,
+  LinearProgress,
 } from "@mui/material";
 import { tableCellClasses } from "@mui/material/TableCell";
 import { Box } from "@mui/system";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+const StyledTableCell = styled(TableCell, {
+  shouldForwardProp: (prop) => (prop === "cellWidth" ? false : true),
+})(({ cellWidth }) => ({
   [`&.${tableCellClasses.head}`]: {
     fontWeight: "bold",
+    width: cellWidth ? cellWidth : undefined,
   },
   [`&.${tableCellClasses.root}`]: { border: 0 },
 }));
@@ -35,33 +41,89 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-const CustomTable = ({ heading, headers, rows, pagination }) => {
+export const TableHeading = ({
+  title,
+  children,
+  titleComponent = "h3",
+  titleSx = { fontSize: "20px", fontWeight: "bold", flexGrow: "1" },
+}) => (
+  <Box
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      px: 2,
+      py: 1.5,
+      borderBottom: (theme) => `1px solid ${theme.palette.highlight.main}`,
+    }}
+  >
+    <Typography component={titleComponent} sx={titleSx}>
+      {title}
+    </Typography>
+    <Box sx={{ display: "flex", gap: 1 }}>{children}</Box>
+  </Box>
+);
+
+const CustomTable = ({
+  heading,
+  headers,
+  rows,
+  pagination,
+  isLoading,
+  isFetching,
+  headerCellWidth,
+  containerProps,
+}) => {
   return (
-    <CustomContainer>
+    <CustomContainer {...containerProps}>
       {heading}
       <TableContainer component={"div"}>
         <Table>
           <TableHead>
             <TableRow>
               {headers.map((header, index) => (
-                <StyledTableCell key={index} component="th">
+                <StyledTableCell
+                  key={index}
+                  component="th"
+                  cellWidth={headerCellWidth}
+                >
                   {header}
                 </StyledTableCell>
               ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row, index) => (
-              <StyledTableRow key={row.id || index} onClick={row.onClick}>
-                {row.items.map((item, index) => (
-                  <StyledTableCell key={index}>{item}</StyledTableCell>
-                ))}
-              </StyledTableRow>
-            ))}
+            {isLoading ? (
+              <>
+                <StyledTableRow>
+                  <StyledTableCell colSpan={headers.length} rowSpan={3}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <CircularProgress />
+                    </Box>
+                  </StyledTableCell>
+                </StyledTableRow>
+                <StyledTableRow />
+                <StyledTableRow />
+              </>
+            ) : (
+              rows.map((row, index) => (
+                <StyledTableRow key={row.id || index} onClick={row.onClick}>
+                  {row.items.map((item, index) => (
+                    <StyledTableCell key={index}>{item}</StyledTableCell>
+                  ))}
+                </StyledTableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </TableContainer>
-      {pagination}
+      {isFetching ? <LinearProgress /> : <Box sx={{ height: "4px" }} />}
+      <Box sx={{ m: 1 }}>{pagination}</Box>
     </CustomContainer>
   );
 };
