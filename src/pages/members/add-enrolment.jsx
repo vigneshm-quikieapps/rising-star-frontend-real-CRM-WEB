a23f9885b5f054b505f3efa20b1b7d60e752b7a0
Pinky <pinky@quikieapps.com>
2022-01-13 18:05:37 +0530

    class session list modal

---
+++ src/pages/members/add-enrolment.jsx
@@ -0,0 +1,288 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  Box,
+  Typography,
+  MenuItem,
+  CircularProgress,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import { ExpandMore as ExpandMoreIcon } from "@mui/icons-material";
+import { Card, HeadingText, SubHeadingText } from "../../components/common";
+import {
+  IconButton,
+  ImgIcon,
+  Grid,
+  Accordion,
+  GradientButton,
+  DatePicker,
+  Button,
+} from "../../components";
+import { TextField, Output } from "../../components/index";
+import { backIcon } from "../../assets/icons";
+import { getMemberEnrolmentList } from "../../redux/action/memberAction";
+import { getClassSessionsByTermId } from "../../redux/action/sessionAction";
+import toPascal from "../../utils/to-pascal";
+import ClassList from "./components/class-lst";
+import SessionList from "./components/session-list";
+import { useGetEnrolment } from "../../services/queries";
+import { useSetError } from "../../contexts/error-context";
+
+const AddEnrolment = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const member = useSelector((state) => state.members.currentMember || {});
+  const businessList = useSelector((state) => state.businesses.businessList);
+  const enrolmentList = useSelector((state) => state.members.enrolmentList);
+  const sessionList = useSelector(
+    (state) => state.sessions.sessionsOfClassInTerm,
+  );
+  const [selectedBusiness, setSelectedBusiness] = useState("");
+  const [selectedEnrolment, setSelectedEnrolment] = useState("");
+  const [selectedSession, setSelectedSession] = useState("");
+  const [date, setDate] = useState(new Date("01-01-2022"));
+  const [showClassList, setShowClassList] = useState(false);
+  const [showSessionList, setShowSessionList] = useState(false);
+  const setError = useSetError();
+
+  const clubMembershipId = useMemo(() => {
+    const list = member?.membership;
+    const membership = list?.find(
+      (mShip) => mShip.businessId === selectedBusiness,
+    );
+    return membership?.clubMembershipId || "";
+  }, [member, selectedBusiness]);
+
+  const { data, isLoading } = useGetEnrolment(selectedSession, {
+    refetchOnWindowFocus: false,
+    onError: (error) => {
+      setError(error);
+    },
+  });
+
+  useEffect(() => {
+    businessList.length && setSelectedBusiness(businessList[0]._id);
+  }, [businessList]);
+
+  useEffect(() => {
+    member?._id &&
+      selectedBusiness &&
+      dispatch(
+        getMemberEnrolmentList({
+          memberId: member._id,
+          businessId: selectedBusiness,
+        }),
+      );
+  }, [dispatch, member, selectedBusiness]);
+  const businessChangeHandler = (e) => setSelectedBusiness(e.target.value);
+
+  const currentEnrolment = useMemo(
+    () =>
+      data?.enrolment || {
+        class: { name: "", charges: [] },
+        session: { name: "", pattern: [], term: { label: "" } },
+      },
+    [data],
+  );
+  const {
+    class: { name: className, charges: classCharges },
+    session: {
+      name: sessionName,
+      pattern,
+      term: { label: termName },
+    },
+    enrolledStatus: status,
+    startDate,
+  } = currentEnrolment;
+  const timings = useMemo(() => {
+    if (!pattern.length) return " - - - ";
+    const days = pattern.map(({ day }) => day).join(", ");
+    const startTime = new Date(pattern[0].startTime).toLocaleTimeString();
+    const endTime = new Date(pattern[0].endTime).toLocaleTimeString();
+    return `${toPascal(days)}, ${startTime} to ${endTime}`.replace(
+      /:00 /g,
+      " ",
+    );
+  }, [pattern]);
+
+  const calcDate = (date) =>
+    date ? new Date(date).toLocaleDateString() : " - - - ";
+
+  const enrolmentDate = calcDate(currentEnrolment?.registeredDate);
+  // const startDate = calcDate(currentEnrolment?.startDate);
+  const lastActionDate = calcDate(currentEnrolment?.updatedAt);
+
+  // useEffect(() => {
+  //   if (enrolmentList.length) {
+  //     setSelectedEnrolment(enrolmentList[0]._id);
+  //     dispatch(
+  //       getClassSessionsByTermId(
+  //         enrolmentList[0].class._id,
+  //         enrolmentList[0].session.term._id,
+  //       ),
+  //     );
+  //   } else {
+  //     setSelectedEnrolment("");
+  //   }
+  // }, [dispatch, enrolmentList]);
+  // const pattern = useMemo(() => {
+  //   if (!currentEnrolment) return "";
+  //   const session = sessionList.find(
+  //     ({ _id }) => _id === currentEnrolment?.sessionId,
+  //   );
+  //   if (!session) return "";
+  //   const days = session.pattern.map(({ day }) => day).join(", ");
+  //   const startTime = new Date(
+  //     session.pattern[0].startTime,
+  //   ).toLocaleTimeString();
+  //   const endTime = new Date(session.pattern[0].endTime).toLocaleTimeString();
+  //   return `${toPascal(days)}, ${startTime} to ${endTime}`.replace(
+  //     /:00 /g,
+  //     " ",
+  //   );
+  // }, [currentEnrolment, sessionList]);
+  const ClassSelectHandler = (id) => {
+    setShowClassList(false);
+    setSelectedEnrolment(id);
+    localStorage.setItem("Id", selectedEnrolment);
+  };
+  const SessionSelectHandler = (id) => {
+    setShowSessionList(false);
+    setSelectedSession(id);
+  };
+
+  if (!member)
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "calc(100vh - 153px)",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center", mb: 1.5 }}>
+        <IconButton onClick={() => history.push("/members")}>
+          <ImgIcon>{backIcon}</ImgIcon>
+        </IconButton>
+        <Typography
+          variant="h3"
+          sx={{ fontSize: "20px", fontWeight: "bold", ml: 1 }}
+        >
+          Add a new enrolment
+        </Typography>
+      </Box>
+      <Card>
+        <HeadingText>{member.name}</HeadingText>
+        <SubHeadingText>Student/Member</SubHeadingText>
+        <Grid>
+          <TextField
+            select
+            variant="filled"
+            label="Business Name"
+            value={selectedBusiness}
+            onChange={businessChangeHandler}
+          >
+            {businessList
+              .filter(({ _id }) =>
+                member?.membership?.some(
+                  ({ businessId }) => businessId === _id,
+                ),
+              )
+              .map(({ _id, name }) => {
+                return (
+                  <MenuItem key={_id} value={_id}>
+                    {name}
+                  </MenuItem>
+                );
+              })}
+          </TextField>
+          <Box />
+          <Output
+            title="Club Membership Number"
+            description={clubMembershipId}
+          />
+        </Grid>
+      </Card>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+            <Typography variant="h2" sx={{ fontSize: "20px", flex: 1 }}>
+              Enrolment Details
+            </Typography>
+          </Box>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid sx={{ gridTemplateColumns: "repeat(3, minmax(0, 1fr))" }}>
+            <GradientButton
+              active
+              sx={{ mr: 2 }}
+              onClick={() => setShowClassList(true)}
+            >
+              Search for a Class
+            </GradientButton>
+            <Output title="Class Name" description={className} />
+            <Output />
+            <GradientButton
+              active
+              sx={{ mr: 2 }}
+              onClick={() => setShowSessionList(true)}
+            >
+              Search for a Session
+            </GradientButton>
+            <Output title="Session Name" description={sessionName} />
+            <Output title="Timings" description={timings} />
+            <Output />
+            <Output
+              title="Term Start Date"
+              description={calcDate(currentEnrolment?.session?.term?.startDate)}
+            />
+            <Output
+              title="Term End Date"
+              description={calcDate(currentEnrolment?.session?.term?.endDate)}
+            />
+            <Output />
+            <Output title="Term" description={termName} />
+
+            <Output
+              title="Member Start Date"
+              description={
+                startDate ? new Date(startDate).toLocaleDateString() : " - - - "
+              }
+            />
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+      <ClassList
+        open={showClassList}
+        onClose={() => setShowClassList(false)}
+        onSelect={ClassSelectHandler}
+        memberId={member?._id}
+        businessId={selectedBusiness}
+        memberName={member?.name}
+      />
+      <SessionList
+        open={showSessionList}
+        onClose={() => setShowSessionList(false)}
+        onSelect={SessionSelectHandler}
+        memberId={member?._id}
+        businessId={selectedBusiness}
+        memberName={member?.name}
+      />
+      <GradientButton>Submit for Enrolment</GradientButton>
+      <Button sx={{ ml: 2 }}>Discard</Button>
+    </>
+  );
+};
+
+export default AddEnrolment;
---
+++ src/pages/members/components/session-list.jsx
@@ -0,0 +1,158 @@
+import { useState, useMemo } from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { Close as CloseIcon } from "@mui/icons-material";
+
+import { enrolmentStatusMap } from "../../../helper/constants";
+import { useGetSession } from "../../../services/queries";
+import { toPascal, transformError } from "../../../utils";
+import {
+  ElevationScroll,
+  Output,
+  Pagination,
+  Table,
+  WarningDialog,
+} from "../../../components";
+
+const EnrolmentsModal = styled(Dialog)(({ theme }) => ({
+  "& .MuiPaper-root": { borderRadius: theme.shape.borderRadiuses.ternary },
+  "& label": { lineHeight: "initial !important" },
+}));
+
+const TableHeading = (
+  <Typography sx={{ fontSize: "20px", fontWeight: "bold", padding: "20px" }}>
+    Enrolment Details
+  </Typography>
+);
+
+const SessionList = ({
+  open,
+  onSelect,
+  onClose,
+  memberId,
+  businessId,
+  memberName,
+}) => {
+  const [showError, setShowError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [contentRef, setContentRef] = useState();
+
+  //   console.log(open, onSelect, onClose, memberId, businessId, memberName);
+
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useGetSession(localStorage.getItem("ID"));
+
+  //   const searchChangeHandler = (e) => setSearchValue(e.target.value);
+
+  const pageChangeHandler = (_, value) => {
+    setPage(value);
+  };
+
+  const tableRows = useMemo(() => {
+    console.log("session", data);
+    return (
+      data?.docs?.map(
+        ({
+          _id,
+          class: { name: className },
+          enrolledStatus,
+          session: {
+            name: sessionName,
+            term: { label: termName },
+          },
+        }) => ({
+          onClick: () => {
+            onSelect(_id, className);
+            onClose();
+          },
+          items: [
+            toPascal(className),
+            toPascal(enrolmentStatusMap[enrolledStatus]),
+            toPascal(termName),
+            toPascal(sessionName),
+          ],
+        }),
+      ) || []
+    );
+  }, [data, onSelect, onClose]);
+
+  const pagination = data?.totalPages && data.totalPages > 1 && (
+    <Pagination
+      count={data.totalPages}
+      disabled={isPreviousData}
+      onChange={pageChangeHandler}
+    />
+  );
+
+  return (
+    <EnrolmentsModal open={open} maxWidth="md">
+      <ElevationScroll targetRef={contentRef}>
+        <DialogTitle
+          sx={{
+            fontSize: "28px",
+            fontWeight: "bold",
+            zIndex: 1,
+          }}
+        >
+          <>
+            Select an Session
+            <Output
+              title="Student Name"
+              description={memberName}
+              sx={{ mb: "10px", mt: "10px" }}
+            />
+          </>
+        </DialogTitle>
+      </ElevationScroll>
+      <IconButton
+        onClick={onClose}
+        sx={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          bgcolor: "ternary.main",
+          zIndex: 1,
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent
+        ref={(e) => setContentRef(e)}
+        sx={{ minWidth: "400px", pt: 0 }}
+      >
+        {isError ? (
+          <WarningDialog
+            open={showError}
+            title="Error"
+            description={transformError(error)}
+            acceptButtonTitle="Discard"
+            onAccept={() => setShowError(false)}
+          />
+        ) : (
+          <Table
+            heading={TableHeading}
+            headers={[
+              "Session Name",
+              "Timing",
+              "Start Date- End Date",
+              "Term Name",
+              "Session Status",
+            ]}
+            rows={tableRows}
+            pagination={pagination}
+            isLoading={isLoading}
+            isFetching={isFetching}
+          />
+        )}
+      </DialogContent>
+    </EnrolmentsModal>
+  );
+};
+
+export default SessionList;
